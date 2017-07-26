a699df76d670fcd5252e8a73475123831da8d5b5
Wolverator <andrey131295@yandex.ru>
2017-07-26 23:20:05 +0300

    Работа наж конструкциями и конструкторами

--- MunchkinBoss/Card.cs
+++ MunchkinBoss/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace MunchkinBoss
 {
     abstract class Card
     {
-        private string _title;
-        private string _text;
-
-        public Card(string ti, string te)
+        private string _title;//название карты
+        private string _text;//текст карты
+        private Image _imagefront, _imageback;
+         /// <summary>
+        /// Создание карты
+        /// </summary>
+        /// <param name="ti">Название карты</param>
+        /// <param name="te">Текст карты</param>
+        public Card(string ti, string te, Image imf, Image imb)
         {
             _title = ti;
             _text = te;
@@ -20,22 +26,33 @@ namespace MunchkinBoss
 
     class Door : Card
     {
-        public Door(string ti, string te) : base(ti, te)
+        /// <summary>
+        /// Создание карты дверей
+        /// </summary>
+        /// <param name="ti">Название карты</param>
+        /// <param name="te">Текст карты</param>
+        public Door(string ti, string te, Image imf, Image imb) : base(ti, te, imf, imb)
         {
         }
     }
 
     class Treasure : Card
     {
-        public Treasure(string ti, string te) : base(ti, te)
+        /// <summary>
+        /// Создание карты сокровищ
+        /// </summary>
+        /// <param name="ti">Название карты</param>
+        /// <param name="te">Текст карты</param>
+        public Treasure(string ti, string te, Image imf, Image imb) : base(ti, te, imf, imb)
         {
         }
     }
 
     class Monster : Door
     {
-        private int _level;
-        public Monster(string ti, string te, int lvl) : base(ti, te)
+        private int _level;//уровень монстра
+        private int _lvls, _trsrs;//награды за его убийство
+        public Monster(string ti, string te, Image imf, Image imb, int lvl) : base(ti, te, imf, imb)
         {
             _level = lvl;
         }
---
+++ MunchkinBoss/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunchkinBoss
+{
+    class Game
+    {
+        private Player[] _players = new Player[6];
+
+        public void Run()
+        {
+            while (true)
+            {
+                foreach (Player p in _players)
+                {
+                    if (p != null)
+                    {
+                        //действия игрока
+                    }
+                }
+            }
+
+        }
+
+    }
+}
